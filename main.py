ea40d173c4fb6a9e4cde62af41cae29f33108b7e
Sudhamsh <msudhamsh06@gmail.com>
2026-07-04 21:50:45 +0530

    change in middleware

--- main.py
+++ main.py
@@ -16,6 +16,7 @@ app.add_middleware(
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
+    expose_headers=["Retry-After"],  # <-- ADD THIS LINE HERE
 )
 
 ORDERS_CATALOG: List[Dict[str, Any]] = [
